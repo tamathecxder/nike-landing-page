c1107351329682bb9d03fc3851f47be6c476a73f
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-06 23:00:50 +0700

    finish styling the navbar component

--- src/components/Nav.jsx
+++ src/components/Nav.jsx
@@ -1,7 +1,32 @@
+import { headerLogo } from "../assets/images";
+import { hamburger } from "../assets/icons";
+import { navLinks } from "../constants";
+
 const Nav = () => {
   return (
-    <div>Nav</div>
-  )
-}
+    <header className="padding-x py-8 absolute z-10 w-full">
+      <nav className="flex justify-between items-center max-container">
+        <a href="/">
+          <img src={headerLogo} alt="Logo" width={130} height={29} />
+        </a>
+        <ul className="flex flex-1 justify-center items-center gap-16 max-lg:hidden">
+          {navLinks.map((item) => (
+            <li key={item.label}>
+              <a
+                href={item.href}
+                className="fotn-montserrat leading-normal text-lg text-slate-gray"
+              >
+                {item.label}
+              </a>
+            </li>
+          ))}
+        </ul>
+        <div className="hidden max-lg:block">
+          <img src={hamburger} alt="Hamburger" width={25} height={25} />
+        </div>
+      </nav>
+    </header>
+  );
+};
 
-export default Nav
+export default Nav;
