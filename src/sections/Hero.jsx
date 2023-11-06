84d3ddc2e793f64681720da028b989a32c5171b2
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-06 23:15:10 +0700

    update style of hero section

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,7 +1,29 @@
+import Button from "../components/Button";
+
+import { arrowRight } from "../assets/icons";
+
 const Hero = () => {
   return (
-    <div>Hero</div>
-  )
-}
+    <section
+      id="home"
+      className="flex flex-col xl:flex-row justify-center min-h-screen gap-10 max-container"
+    >
+      <div className="relative xl:w-2/5 flex flex-col justify-center items-start w-full max-xl:padding-x pt-28">
+        <p>Our Summer Collections</p>
+        <h1>
+          <span>The New Arrival</span>
+          <br />
+          <span>Nike</span>
+          Shoes
+        </h1>
+        <p>
+          Discover stylish Nike arrivals, quality comfort, and innovation for
+          your active life
+        </p>
+        <Button label="Shop now" iconURL={arrowRight} />
+      </div>
+    </section>
+  );
+};
 
-export default Hero
+export default Hero;
