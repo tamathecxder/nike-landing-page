34df383548a48e46a0db513149d57f0a8f56f578
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-06 22:22:24 +0700

    add tags to each text component name

--- src/App.jsx
+++ src/App.jsx
@@ -10,30 +10,30 @@ import {
 
 const App = () => (
   <main className="relative">
-    Nav
+    {/* <Nav /> */}
     <section className="xl:padding-1 wide:padding-r padding-b">
-      Hero
+      <Hero />
     </section>
     <section className="padding">
-      PopularProducts
+      <PopularProducts />
     </section>
     <section className="padding">
-      SuperQuality
+      <SuperQuality />
     </section>
     <section className="padding padding-x py-10">
-      Services
+      <Services />
     </section>
     <section className="padding">
-      SpecialOffer
+      <SpecialOffer />
     </section>
     <section className="padding bg-pale-blue">
-      CustomerReviews
+      <CustomerReviews />
     </section>
     <section className="padding padding-x sm:py-32 py-16 w-full">
-      Subscribe
+      <Subscribe />
     </section>
     <section className="padding bg-black padding-x padding-t pb-8">
-      Footer
+      <Footer />
     </section>
   </main>
 )
