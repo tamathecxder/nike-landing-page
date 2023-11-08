4ce83869580b6bfd64721d4c4b13e70b1c25c15e
tamathecxder <yudistiraeka.pratama012@gmail.com>
2023-11-09 00:01:48 +0700

    fix hero section issue

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,7 +6,7 @@ import ShoeCard from "../components/ShoeCard";
 import { useState } from "react";
 
 const Hero = () => {
-  const [bigShoeImage, setbigShoeImage] = useState(bigShoe1);
+  const [bigShoeImage, setBigShoeImage] = useState(bigShoe1);
 
   return (
     <section
@@ -17,7 +17,7 @@ const Hero = () => {
         <p className="text-xl font-montserrat text-coral-red">
           Our Summer Collections
         </p>
-        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82] font-bold">
+        <h1 className="mt-10 font-palanquin text-8xl max-sm:text-[72px] max-sm:leading-[82px] font-bold">
           <span className="xl:bg-white xl:whitespace-nowrap relative z-10 pr-10">
             The New Arrival
           </span>
@@ -58,7 +58,7 @@ const Hero = () => {
                 imgURL={shoe}
                 bigShoeImage={bigShoeImage}
                 changeBigShoeImage={(shoe) => {
-                  setbigShoeImage(shoe);
+                  setBigShoeImage(shoe);
                 }}
               />
             </div>
